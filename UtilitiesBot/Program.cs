18ae3cb06bb8de34646d255d8f1061d5e1f4e11f
0x49D1 <pursanovd@gmail.com>
2016-12-07 21:16:44 +0400

    Added ability to encode/decode url

--- UtilitiesBot/Program.cs
+++ UtilitiesBot/Program.cs
@@ -80,127 +80,141 @@ namespace UtilitiesBot
 
             string resMessage = "Nothing found for command. Try /help";
             IInstantAnswer instantAnswer = null;
-            try 
+            try
             {
-            if (msg.StartsWithOrdinalIgnoreCase("/tounixtime;/toepoch"))
-            {
-                instantAnswer = new UnixTimeStampInstantAnswer();
-            }
-            if (msg.StartsWithOrdinalIgnoreCase("/formatjson;/jsonformat"))
-            {
-                try
+                if (msg.StartsWithOrdinalIgnoreCase("/encodeurl;/urlencode"))
                 {
-                    bool wasEscaped = false;
-
                     string value = msg.RemoveCommandPart().Trim();
-                    if (value.Contains("\\\""))
-                        wasEscaped = true;
-                    value = value.Replace("\\\"", "\"");
-                    dynamic parsedJson = JsonConvert.DeserializeObject(value);
-
-                    resMessage = JsonConvert.SerializeObject(parsedJson, Formatting.Indented); // todo needs refactor
-                    if (wasEscaped)
-                        resMessage = resMessage.Replace("\"", "\\\"");
+                    resMessage = HttpUtility.UrlEncode(value);
                 }
-                catch (Exception ex)
+                if (msg.StartsWithOrdinalIgnoreCase("/decodeurl;/urldecode"))
                 {
-                    logger.Error(ex);
-                    resMessage = "Probably JSON was not correct";
+                    string value = msg.RemoveCommandPart().Trim();
+                    resMessage = HttpUtility.UrlDecode(value);
                 }
-            }
-            if (msg.StartsWithOrdinalIgnoreCase("/iplocation;/geolocation;/ip"))
-            {
-                string value = HttpUtility.UrlEncode(msg.RemoveCommandPart().Trim());
-
-                if (!string.IsNullOrEmpty(value) && !Regex.IsMatch(value, @"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
-                    resMessage = "Wrong ip format!";
-                else if (string.IsNullOrEmpty(value))
-                    resMessage = "You can check your ip here: https://ipinfo.io/ip";
-                else
+                if (msg.StartsWithOrdinalIgnoreCase("/tounixtime;/toepoch"))
                 {
-                    //http://ip-api.com/json/$ip
-                    var httpClient = new HttpClient();
-                    var response = await httpClient.GetAsync("http://ip-api.com/json/" + value);
-                    bool? timeout = cache.Get("iplocationtrytimeout") as bool?; // only 150 per minute allowed
-                    if (timeout != null)
-                        locationTryCount++;
-                    else
+                    instantAnswer = new UnixTimeStampInstantAnswer();
+                }
+                if (msg.StartsWithOrdinalIgnoreCase("/formatjson;/jsonformat"))
+                {
+                    try
                     {
-                        locationTryCount = 0;
-                        cache.Set("iplocationtrytimeout", true,
-                            new CacheItemPolicy() { AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(1) });
+                        bool wasEscaped = false;
+
+                        string value = msg.RemoveCommandPart().Trim();
+                        if (value.Contains("\\\""))
+                            wasEscaped = true;
+                        value = value.Replace("\\\"", "\"");
+                        dynamic parsedJson = JsonConvert.DeserializeObject(value);
+
+                        resMessage = JsonConvert.SerializeObject(parsedJson, Formatting.Indented); // todo needs refactor
+                        if (wasEscaped)
+                            resMessage = resMessage.Replace("\"", "\\\"");
                     }
-                    if (locationTryCount > 100)
-                        resMessage = "Try count exceeded, retry later";
-                    else
+                    catch (Exception ex)
                     {
-                        string content = await response.Content.ReadAsStringAsync();
-                        dynamic parsedJson = JsonConvert.DeserializeObject(content);
-                        resMessage = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
-                        JObject jo = JObject.Parse(content);
-                        string country = jo.SelectToken("countryCode").ToString();
-                        if (!string.IsNullOrEmpty(country))
-                            resMessage += "\nhttp://icons.iconarchive.com/icons/famfamfam/flag/16/" + country.ToLower() + "-icon.png";
+                        logger.Error(ex);
+                        resMessage = "Probably JSON was not correct";
                     }
                 }
-            }
-            if (msg.StartsWithOrdinalIgnoreCase("/ddg;/duckduckgo;/duckduckgoinstant"))
-            {
-                string value = HttpUtility.UrlEncode(msg.RemoveCommandPart().Trim());
-                if (!string.IsNullOrEmpty(value))
+                if (msg.StartsWithOrdinalIgnoreCase("/iplocation;/geolocation;/ip"))
                 {
-                    //http://api.duckduckgo.com/?q=14ml%20in%20litre&format=json
-                    var httpClient = new HttpClient();
-                    var response = await httpClient.GetAsync("https://api.duckduckgo.com/?q=" + value + "&format=json");
-                    string content = await response.Content.ReadAsStringAsync();
-                    JObject jo = JObject.Parse(content);
-                    string answer = Regex.Replace(jo.SelectToken("Answer").ToString(), @"<[^>]*>", String.Empty);
-                    if (string.IsNullOrEmpty(answer) || answer.Contains(" IP "))// Your IP address is xxx in xxx
+                    string value = HttpUtility.UrlEncode(msg.RemoveCommandPart().Trim());
+
+                    if (!string.IsNullOrEmpty(value) && !Regex.IsMatch(value, @"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
+                        resMessage = "Wrong ip format!";
+                    else if (string.IsNullOrEmpty(value))
+                        resMessage = "You can check your ip here: https://ipinfo.io/ip";
+                    else
                     {
-                        string moreAnswer = jo.SelectToken("RelatedTopics").Any() ? jo.SelectToken("RelatedTopics")[0]["Result"].ToString() : "";
-                        if (!string.IsNullOrEmpty(moreAnswer) && moreAnswer.Contains("</a>") && moreAnswer.IndexOf("</a>", StringComparison.OrdinalIgnoreCase) + 4 < moreAnswer.Length)
-                        {
-                            moreAnswer = moreAnswer.Substring(moreAnswer.IndexOf("</a>", StringComparison.OrdinalIgnoreCase) + 4);
-                            if (!string.IsNullOrEmpty(moreAnswer))
-                                resMessage = moreAnswer + "\n" + jo.SelectToken("RelatedTopics")[0]["Icon"]["URL"] +
-                                             "\n" + "See: https://duckduckgo.com/?q=" + value;
-                        }
+                        //http://ip-api.com/json/$ip
+                        var httpClient = new HttpClient();
+                        var response = await httpClient.GetAsync("http://ip-api.com/json/" + value);
+                        bool? timeout = cache.Get("iplocationtrytimeout") as bool?; // only 150 per minute allowed
+                        if (timeout != null)
+                            locationTryCount++;
                         else
                         {
-                            disableMessagePreview = true;
-                            resMessage =
-                                "Instant not found. Try the following multi searches:\nGoogle: https://google.com/search?q=" + value +
-                                "\nDuckduckgo: https://duckduckgo.com/?q=" + value +
-                                "\nYandex: https://yandex.ru/search/?text=" + value +
-                                "\nGitHub: https://github.com/search?utf8=%E2%9C%93&q=" + value +
-                                "\nWikipedia: https://en.wikipedia.org/wiki/Special:Search?search=" + value +
-                                "\nWolframAlpha: https://www.wolframalpha.com/input/?i=" + value;
+                            locationTryCount = 0;
+                            cache.Set("iplocationtrytimeout", true,
+                                new CacheItemPolicy() { AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(1) });
+                        }
+                        if (locationTryCount > 100)
+                            resMessage = "Try count exceeded, retry later";
+                        else
+                        {
+                            string content = await response.Content.ReadAsStringAsync();
+                            dynamic parsedJson = JsonConvert.DeserializeObject(content);
+                            resMessage = JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+                            JObject jo = JObject.Parse(content);
+                            string country = jo.SelectToken("countryCode").ToString();
+                            if (!string.IsNullOrEmpty(country))
+                                resMessage += "\nhttp://icons.iconarchive.com/icons/famfamfam/flag/16/" + country.ToLower() + "-icon.png";
                         }
                     }
-                    else
-                        resMessage = answer;
                 }
-            }
-            if (msg.StartsWithOrdinalIgnoreCase("/hash"))
-            {
-                instantAnswer = new HashCalculatorInstantAnswer();
-            }
-            else if (message.Text.StartsWith("/start") || message.Text.StartsWith("/help"))
-            {
-                resMessage = @"Usage:
+                if (msg.StartsWithOrdinalIgnoreCase("/ddg;/duckduckgo;/duckduckgoinstant"))
+                {
+                    string value = HttpUtility.UrlEncode(msg.RemoveCommandPart().Trim());
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        //http://api.duckduckgo.com/?q=14ml%20in%20litre&format=json
+                        var httpClient = new HttpClient();
+                        var response = await httpClient.GetAsync("https://api.duckduckgo.com/?q=" + value + "&format=json");
+                        string content = await response.Content.ReadAsStringAsync();
+                        JObject jo = JObject.Parse(content);
+                        string answer = Regex.Replace(jo.SelectToken("Answer").ToString(), @"<[^>]*>", String.Empty);
+                        if (string.IsNullOrEmpty(answer) || answer.Contains(" IP "))// Your IP address is xxx in xxx
+                        {
+                            string moreAnswer = jo.SelectToken("RelatedTopics").Any() ? jo.SelectToken("RelatedTopics")[0]["Result"].ToString() : "";
+                            if (!string.IsNullOrEmpty(moreAnswer) && moreAnswer.Contains("</a>") && moreAnswer.IndexOf("</a>", StringComparison.OrdinalIgnoreCase) + 4 < moreAnswer.Length)
+                            {
+                                moreAnswer = moreAnswer.Substring(moreAnswer.IndexOf("</a>", StringComparison.OrdinalIgnoreCase) + 4);
+                                if (!string.IsNullOrEmpty(moreAnswer))
+                                    resMessage = moreAnswer + "\n" + jo.SelectToken("RelatedTopics")[0]["Icon"]["URL"] +
+                                                 "\n" + "See: https://duckduckgo.com/?q=" + value;
+                            }
+                            else
+                            {
+                                disableMessagePreview = true;
+                                resMessage =
+                                    "Instant not found. Try the following multi searches:\nGoogle: https://google.com/search?q=" + value +
+                                    "\nDuckduckgo: https://duckduckgo.com/?q=" + value +
+                                    "\nYandex: https://yandex.ru/search/?text=" + value +
+                                    "\nGitHub: https://github.com/search?utf8=%E2%9C%93&q=" + value +
+                                    "\nWikipedia: https://en.wikipedia.org/wiki/Special:Search?search=" + value +
+                                    "\nWolframAlpha: https://www.wolframalpha.com/input/?i=" + value;
+                            }
+                        }
+                        else
+                            resMessage = answer;
+                    }
+                }
+                if (msg.StartsWithOrdinalIgnoreCase("/hash"))
+                {
+                    instantAnswer = new HashCalculatorInstantAnswer();
+                }
+                else if (message.Text.StartsWith("/start") || message.Text.StartsWith("/help"))
+                {
+                    resMessage = @"Usage:
 Default command is /ddg
 /help - Shows all the commands with examples for some of them
 /ddg - Instant answers from duckduckgo.com. Example: /ddg 15km to miles
-/ip - Information about selected ip address (location, etc)
-/tounixtime - Convert datetime to unixtimestamp. Message must be like in format: dd.MM.yyyy HH:mm:sss 01.09.1980 06:32:32. Or just text 'now'
-/toepoch - Calculate unix timestamp for date in format dd.MM.yyyy HH:mm:ss
+/ip - Information about selected ip address (location, etc). Example /ip xxx.xxx.xxx.xxx
+/formatjson - Reformats provided JSON string into pretty idented string
+/tounixtime - Convert datetime to unixtimestamp. Message must be like in format: dd.MM.yyyy HH:mm:ss 01.09.1980 06:32:32. Or just text 'now'
 /hash - Calculate hash. Use like this: /hash sha256 test
+/urlencode - URL-encodes a string and returns the encoded string.
+/urldecode - Decodes URL-encoded string
 ";
-            }
+                }
 
-            if (instantAnswer != null)
-                resMessage = instantAnswer.GetInstantAnswer(msg.RemoveCommandPart().Trim());
-            } catch(Exception globalEx){
+                if (instantAnswer != null)
+                    resMessage = instantAnswer.GetInstantAnswer(msg.RemoveCommandPart().Trim());
+            }
+            catch (Exception globalEx)
+            {
                 logger.Error(globalEx); // Ignore any global exception
                 resMessage = "Something went wrong, try another request.";
             }
@@ -215,7 +229,7 @@ Default command is /ddg
                 resMessage = resMessage.Replace(tt, "xx");
             }
 
-            await Bot.SendTextMessageAsync(message.Chat.Id, resMessage,disableMessagePreview);
+            await Bot.SendTextMessageAsync(message.Chat.Id, resMessage, disableMessagePreview);
         }
 
         private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
